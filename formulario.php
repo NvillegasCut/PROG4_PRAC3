2736bdeb4f5500139a6fb4d960284a919ce968f7
nicolas villegas lucio <nicolas villegas lucio>
2016-04-12 23:06:45 -0500

    PRACTICA 3

---
+++ formulario.php
@@ -0,0 +1,17 @@
+<html>
+ <body>
+  <form action="suma.php"
+   method="POST">
+  <label>NUMERO 1:</label>
+  <br><br>
+  <input type="text"
+   name="num_1">
+ <br><br>
+ <label>NUMERO 2:</label>
+  <br><br>
+  <input type="text" name="num_2">
+   <br><br>
+  <input type="submit"
+   value="CALCULAR">
+ </body>
+</html>
---
+++ suma.php
@@ -0,0 +1,9 @@
+<?php
+ $numero1=$_POST["num_1"];
+ $numero2=$_POST["num_2"];
+
+$resultado = $numero1 + $numero2;
+
+echo $resultado;
+
+?>
